61598484acc4701138b43e80b5db944d8bfe6346
aroy23 <arnavroy@ucla.edu>
2024-04-20 12:38:13 -0700

    first commit

---
+++ flask-server/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, port=4999)
